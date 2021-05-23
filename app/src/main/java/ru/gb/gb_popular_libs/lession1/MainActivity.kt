b0187c5acb20c29d9656c7d4751d8a81c3991927
Peter Bukhal <bukhal@vsk.ru>
2021-05-23 15:31:40 +0300

    Исходный пример из урока 1 (c MVP)

--- app/src/main/java/ru/gb/gb_popular_libs/lession1/MainActivity.kt
+++ app/src/main/java/ru/gb/gb_popular_libs/lession1/MainActivity.kt
@@ -1,14 +1,14 @@
 package ru.gb.gb_popular_libs.lession1
 
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import ru.gb.gb_popular_libs.lession1.R.layout.activity_main
 
-class MainActivity : AppCompatActivity(activity_main) {
+class MainActivity : AppCompatActivity(activity_main), MainView {
 
-    val counters = mutableListOf(0, 0, 0)
+    private val presenter = MainPresenter(this)
 
     private val btn_counter1: Button by lazy { findViewById(R.id.btn_counter1) }
     private val btn_counter2: Button by lazy { findViewById(R.id.btn_counter2) }
@@ -17,45 +17,22 @@ class MainActivity : AppCompatActivity(activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        btn_counter1.setOnClickListener {
-            btn_counter1.text = (++counters[0]).toString()
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
         }
 
-        btn_counter2.setOnClickListener {
-            btn_counter2.text = (++counters[1]).toString()
+        btn_counter1.setOnClickListener(listener)
+        btn_counter2.setOnClickListener(listener)
+        btn_counter3.setOnClickListener(listener)
+    }
+
+    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
+    override fun setButtonText(index: Int, text: String) {
+        when(index){
+            0 -> btn_counter1.text = text
+            1 -> btn_counter2.text = text
+            2 -> btn_counter3.text = text
         }
-
-        btn_counter3.setOnClickListener {
-            btn_counter3.text = (++counters[2]).toString()
-        }
-
-        initViews()
-    }
-
-    fun initViews(){
-        btn_counter1.text = counters[0].toString()
-        btn_counter2.text = counters[1].toString()
-        btn_counter3.text = counters[2].toString()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putIntArray("counters", counters.toIntArray())
-    }
-
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        outState.putIntArray("counters", counters.toIntArray())
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        val countersArray = savedInstanceState.getIntArray("counters")
-        countersArray?.toList()?.let {
-            counters.clear()
-            counters.addAll(it)
-        }
-        initViews()
     }
 
 }
