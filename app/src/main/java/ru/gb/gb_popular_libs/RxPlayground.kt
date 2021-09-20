2acd34854e87818f89f203d651ac3177e5b0a5f2
Peter Bukhal <peter.bukhal@gmail.com>
2021-09-20 21:45:00 +0300

    Урок 3. Реактивное программирование, RxJava 2

--- app/src/main/java/ru/gb/gb_popular_libs/RxPlayground.kt
+++ app/src/main/java/ru/gb/gb_popular_libs/RxPlayground.kt
@@ -1,66 +1,34 @@
 package ru.gb.gb_popular_libs
 
-import io.reactivex.*
-import java.lang.RuntimeException
-import java.util.concurrent.TimeUnit.SECONDS
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit.MILLISECONDS
+import kotlin.random.Random
 
-fun nonRx(): Int = 1
+const val article = """Реактивное программирование обеспечивает доступ к асинхронному программированию. Оно используется, чтобы упростить асинхронную обработку длительных операций. Именно это программирование — способ обработки нескольких событий, ошибок и завершения потока событий. Такой тип программирования обеспечивает также упрощённый способ запуска различных задач в разных потоках."""
+val random = Random(System.currentTimeMillis())
 
 fun main() {
-    val disposable =
-        Observable
-            .just(1,2,3)
-            .doOnSubscribe { System.out.println("onSubscribe") }
-            .doOnNext {  }
-            .doOnNext {  }
-            .doOnNext {  }
-            .doOnError {  }
-            .doOnComplete {  }
-//            .subscribe({ counter ->
-//                System.out.println("onNext: $counter")
-//            }, { error ->
-//                System.out.println("onError: $error")
-//            }, { //
-//                System.out.println("onComplete")
-//            })
+    Observable
+        .just(article)
+        .map { text -> text.split(" ") }
+        .concatMap { words -> Observable.fromIterable(words) }
+        .map(String::lowercase)
+        .flatMap(::clearAllNonLetterChars)
+        .subscribeOn(Schedulers.computation())
+        .subscribe(::onWord, ::onError)
 
-    // Прерываем подписку на источник
-    //disposable.dispose()
-
-    // Завершается без эмита событий (данных)
-    Observable.empty<Int>()
-
-    Observable.fromArray(arrayOf(1, 1, 1))
-
-    Observable.fromIterable(listOf(1, 1, 1))
-
-    Observable.timer(10L, SECONDS)
-
-    // Для оборачивания синхронного кода
-    Observable.fromCallable { nonRx() }
-
-    // Для оборачивания асинхронного кода
-    // обработка ошибок ручная
-    Observable.create<Int> { emitter ->
-        try {
-            emitter.onNext(1)
-        } catch (error: Throwable) {
-            emitter.onError(error)
-        } finally {
-            emitter.onComplete()
-        }
-    }
-    //Flowable
-
-
-    Single.error<Int>(RuntimeException("dasd"))
-        .subscribe({ counter ->
-            System.out.println("onSuccess: $counter")
-        }, { error ->
-            System.out.println("onError: $error")
-        })
-    //Maybe
-
-    Completable.complete()
-    Completable.timer(10L, SECONDS)
+    Thread.sleep(15000)
 }
+
+fun clearAllNonLetterChars(word: String): Observable<String> =
+    Observable.fromIterable(word.asIterable())
+        .filter(Char::isLetter)
+        .observeOn(Schedulers.newThread())
+        .reduce(StringBuilder(), { stringBuilder: StringBuilder, char: Char -> stringBuilder.append(char) })
+        .map(StringBuilder::toString)
+        .delay(random.nextLong(500), MILLISECONDS, Schedulers.io())
+        .toObservable()
+
+fun onWord(word: String) = println(word)
+fun onError(error: Throwable) = print(error.localizedMessage)
