28aef1437ed876e148782d5de43734bf04ac7bad
Peter Bukhal <peter.bukhal@gmail.com>
2021-11-11 21:36:19 +0300

    Урок 3. Реактивное программирование, RxJava 2

--- app/src/main/java/ru/gb/gb_popular_libs/RxPlayground.kt
+++ app/src/main/java/ru/gb/gb_popular_libs/RxPlayground.kt
@@ -1,16 +1,25 @@
 package ru.gb.gb_popular_libs
 
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.*
 import io.reactivex.rxjava3.schedulers.Schedulers
 import java.util.concurrent.TimeUnit.MILLISECONDS
 import kotlin.random.Random
 
-const val article = """Реактивное программирование обеспечивает доступ к асинхронному программированию. Оно используется, чтобы упростить асинхронную обработку длительных операций. Именно это программирование — способ обработки нескольких событий, ошибок и завершения потока событий. Такой тип программирования обеспечивает также упрощённый способ запуска различных задач в разных потоках."""
+const val article1 = """Реактивное программирование обеспечивает доступ к асинхронному программированию. Оно используется, чтобы упростить асинхронную обработку длительных операций. Именно это программирование — способ обработки нескольких событий, ошибок и завершения потока событий. Такой тип программирования обеспечивает также упрощённый способ запуска различных задач в разных потоках."""
+const val article2 = """Оператор flatMap похож на map, также применяет функцию к каждому излучаемому элементу, но эта функция возвращает Obsevable. То есть один излучаемый элемент исходного источника через flatMap порождает другие. Проще говоря, flatMap из каждого элемента создаёт новый источник, после чего выполняет слияние этих источников, похожее на применение над ними оператора merge. Разберёмся на примерах."""
 val random = Random(System.currentTimeMillis())
+
+fun createText(): String = ""
 
 fun main() {
     Observable
-        .just(article)
+        .merge(
+            Observable.concat(
+                Observable.just(article1),
+                Observable.just(article2)
+            ),
+            Observable.fromCallable(::createText),
+        )
         .map { text -> text.split(" ") }
         .concatMap { words -> Observable.fromIterable(words) }
         .map(String::lowercase)
